563ac42dd3d4360a9795805423bc558d6d552aeb
djres88 <davidj.resnick@gmail.com>
2016-05-19 15:43:22 -0700

    Minified fix.

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/nineto.jsx",
+  output: {
+    path: './app/assets/javascripts',
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  plugins:[
+   new webpack.DefinePlugin({
+     'process.env':{
+       'NODE_ENV': JSON.stringify('production')
+     }
+   }),
+   new webpack.optimize.UglifyJsPlugin({
+     compress:{
+       warnings: true
+     }
+   })
+ ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
